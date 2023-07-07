dddc656ca3b205e8a9c035dd87a7fa41c084eedf
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2023-07-07 11:42:10 -0300

    feat: "basic wifgets finished"

--- AppConsversion.py
+++ AppConsversion.py
@@ -1,1 +1,25 @@
 import tkinter as tk
+from tkinter import ttk #Here, Tk themed widget set
+
+# window project
+window = tk.Tk()
+
+# Custom window
+window.title('Kauan') #title project
+window.geometry('300x100') #width x height
+
+# Title
+title_label = ttk.Label(master = window, text = 'Miles to kilometers', font = 'Calibri 24 bold')
+title_label.pack()
+
+#input field
+input_frame = ttk.Frame(master = window)
+entry = ttk.Entry(master = input_frame)
+button = ttk.Button(master = input_frame, text = 'Convert')
+entry.pack()
+button.pack()
+input_frame.pack()
+
+
+# run 
+window.mainloop()
