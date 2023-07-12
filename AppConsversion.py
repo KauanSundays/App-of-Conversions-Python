27c0bb54eedeb54291130469ca5854e15af242d6
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2023-07-12 18:40:42 -0300

    feat: "Add Output in project"

--- AppConsversion.py
+++ AppConsversion.py
@@ -6,7 +6,7 @@ window = tk.Tk()
 
 # Custom window
 window.title('Kauan') #title project
-window.geometry('300x100') #width x height
+window.geometry('300x150') #width x height
 
 # Title
 title_label = ttk.Label(master = window, text = 'Miles to kilometers', font = 'Calibri 24 bold')
@@ -16,10 +16,14 @@ title_label.pack()
 input_frame = ttk.Frame(master = window)
 entry = ttk.Entry(master = input_frame)
 button = ttk.Button(master = input_frame, text = 'Convert')
-entry.pack()
-button.pack()
-input_frame.pack()
+entry.pack(side = 'left', padx = 10)
+button.pack(side = 'left')
+input_frame.pack(pady = 10)
 
+# output / result
+
+output_label = ttk.Label(master = window, text= 'Output', font = 'Calibri 24 bold')
+output_label.pack(pady = 5)
 
 # run 
 window.mainloop()
