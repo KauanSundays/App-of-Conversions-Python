4d8a039f7f26c275b2d1b9b7e3f8997a1d92efb0
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2023-07-12 23:54:30 -0300

    feat: "Add Toggle button"

--- AppConsversion.py
+++ AppConsversion.py
@@ -1,47 +1,50 @@
 import tkinter as tk
-from tkinter import ttk #Here, Tk themed widget set
+from tkinter import ttk
+
+def toggle_theme():
+    current_theme = window.tk.call('ttk::style', 'theme', 'use')
+    if current_theme == 'default':
+        window.tk.call('ttk::style', 'theme', 'use', 'clam')
+    else:
+        window.tk.call('ttk::style', 'theme', 'use', 'default')
 
 def convert():
-    # Obtém o valor digitado pelo usuário no campo de entrada e converte para um número inteiro
     number = int(entry.get())
-    
-    # Multiplica o número por 2
     numberVsnumber = number * 2
-    
-    # Define o valor da variável output_string como o resultado da multiplicação
     output_string.set(numberVsnumber)
-
-
 
 # window project
 window = tk.Tk()
 
 # Custom window
-window.title('Kauan') #title project
-window.geometry('320x170') #width x height
+window.title('Kauan')
+window.geometry('320x170')
 
 # Title
-title_label = ttk.Label(master = window, text = 'Double of The number', font = 'Calibri 24 bold')
+title_label = ttk.Label(master=window, text='Double of The number', font='Calibri 24 bold')
 title_label.pack()
 
-#input field
-input_frame = ttk.Frame(master = window)
+# Input field
+input_frame = ttk.Frame(master=window)
 entry_int = tk.IntVar()
-entry = ttk.Entry(master = input_frame)
-button = ttk.Button(master = input_frame, text = 'Double', command=convert) #like "Function of others languages"
-entry.pack(side = 'left', padx = 10)
-button.pack(side = 'left')
-input_frame.pack(pady = 10)
+entry = ttk.Entry(master=input_frame)
+button = ttk.Button(master=input_frame, text='Double', command=convert)
+entry.pack(side='left', padx=10)
+button.pack(side='left')
+input_frame.pack(pady=10)
 
-# output / result
-
+# Output / Result
 output_string = tk.StringVar()
 output_label = ttk.Label(
-    master = window, 
-    text= 'Output', 
-    font = 'Calibri 24 bold', 
-    textvariable= output_string)
-output_label.pack(pady = 5)
+    master=window,
+    text='Output',
+    font='Calibri 24 bold',
+    textvariable=output_string)
+output_label.pack(pady=5)
 
-# run 
+# Toggle theme button
+toggle_button = ttk.Button(master=window, text='Toggle Theme', command=toggle_theme)
+toggle_button.pack(pady=10)
+
+# Run
 window.mainloop()
